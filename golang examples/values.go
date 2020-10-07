0bb13faeb323fabbc46478000e61a9fec5ca5a56
sai kadiveti <saikumar0303@gmail.com>
2020-10-07 20:49:20 +0530

    example2values

---
+++ golang examples/values.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("my name" + " is go" + " stupid")
+	fmt.Println("Dont you know 1+1 is ", 1+1)
+	fmt.Println("Its not to late to start go ")
+	fmt.Println("I always wonder why people say 0 and 1 in computers", true && true)
+	fmt.Println("Sorry out of words", true || false)
+	fmt.Println("People don't confuse me with ! or not", !true)
+}
