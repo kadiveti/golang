02be473f550af91b3069d253088341618a1ea91e
sai kadiveti <saikumar0303@gmail.com>
2020-10-08 19:57:08 +0530

    variables.go

---
+++ golang examples/variable.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+var a = "Just start the variables"
+
+var b, c int = 2, 3
+
+var f = true
+
+var d int
+
+func main() {
+	e := "start"
+	x := 1
+	fmt.Println("The result for x is", x)
+	fmt.Println("The result for a is", a)
+	fmt.Println("The result for b and c is ", b, c)
+	fmt.Println("The result for f is ", f)
+	fmt.Println("The result for e is ", e)
+	fmt.Println("The result for d is ", d)
+}
