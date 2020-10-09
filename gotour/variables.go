3796467e787b6ef8fa4aca231b03de6fe93233ea
sai kadiveti <saikumar0303@gmail.com>
2020-10-09 16:54:52 +0530

    variables

---
+++ gotour/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+// Default it takes 0, empty strings(" ") , false
+
+func main() {
+	var i int
+	var a string
+	fmt.Println(i, a, c, python, java, a)
+}
