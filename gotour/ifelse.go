ca9882146567271792f32db7a1da7c7f697fb5ab
sai kadiveti <saikumar0303@gmail.com>
2020-10-10 15:27:39 +0530

    if else

---
+++ gotour/ifelse.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Both calls to pow return their results before the call to fmt.Println in main begins
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
