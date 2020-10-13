5a06f8865f8d0c0cee943266d157e37a985b7a10
sai kadiveti <saikumar0303@gmail.com>
2020-10-13 20:34:24 +0530

    sliceoslice

---
+++ gotour/names.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 5)
+	printslice("a", a)
+
+	b := make([]int, 0, 5)
+	printslice("b", b)
+
+	c := b[:2]
+	printslice("c", c)
+
+	d := c[2:5]
+	printslice("d", d)
+}
+
+func printslice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
